502f4dd803e820bedd9feda7b79fece5c4a9a743
EMZemskova17 <katuusha0@yandex.ru>
2025-09-22 10:58:19 +0300

    1st: change commit

--- math.go
+++ math.go
@@ -1,21 +1,14 @@
 package main
 
-// Add возвращает сумму
 func Add(a, b int) int {
 	return a + b
 }
-
-// Sub возвращает разность
 func Sub(a, b int) int {
 	return a - b
 }
-
-// Mul возвращает произведение
 func Mul(a, b int) int {
 	return a * b
 }
-
-// Div возвращает частное (если b != 0)
 func Div(a, b int) float64 {
 	if b == 0 {
 		return 0
