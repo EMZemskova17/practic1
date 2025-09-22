f7a3b8879e354d5faf8cf96bbfa3f09e8846c8c3
EMZemskova17 <katuusha0@yandex.ru>
2025-09-22 10:41:49 +0300

    2nd: change commit

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 func main() {
+	//Получение значений, введенных пользователем
 	a, b := GetInput()
 
 	sum := Add(a, b)
-	diff := Sub(a, b)
 	prod := Mul(a, b)
+	diff := Sub(a, b)
 	quot := Div(a, b)
 
 	PrintResults(a, b, sum, diff, prod, quot)
