81e8d4cd5b2e927a82bbcae6441cb2924fdb065b
EMZemskova17 <katuusha0@yandex.ru>
2025-10-26 18:55:39 +0300

    new commit from 2nd

--- feature2.go
+++ feature2.go
@@ -5,5 +5,5 @@ import "fmt"
 func Feature2() {
 	fmt.Println("Новая функциональность в ветке dev2")
 	fmt.Println("Небольшое исправление в ветке dev2")
-
+	fmt.Println("Для создани нового коммита")
 }
