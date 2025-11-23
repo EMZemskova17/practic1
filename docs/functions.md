af3de9e78dca451200e19840898efe13d8d8deed
EMZemskova17 <katuusha0@yandex.ru>
2025-11-23 18:54:27 +0300

    docs add

---
+++ docs/functions.md
@@ -0,0 +1,11 @@
+# Practic1 Project
+
+## Описание
+Простейший проект на Go с функциями Add, Mult, Pow, Div, Sub.
+
+## Функции
+- `Add(a, b int) int` — сложение двух чисел
+- `Mult(a, b int) int` — умножение двух чисел
+- `Pow(base, exp int) int` — возведение числа в степень
+- `Sub(a, b int) int` — вычитание двух чисел
+- `Div(base, exp int) int` — деление двух чисел
--- math.go
+++ math.go
@@ -1,14 +1,21 @@
 package main
 
+//Сложение двух чисел
 func Add(a, b int) int {
 	return a + b
 }
+
+//Вычитание двух чисел
 func Sub(a, b int) int {
 	return a - b
 }
+
+//Умножение двух чисел
 func Mul(a, b int) int {
 	return a * b
 }
+
+//Деление двух чисел
 func Div(a, b int) float64 {
 	if b == 0 {
 		return 0
@@ -16,6 +23,7 @@ func Div(a, b int) float64 {
 	return float64(a) / float64(b)
 }
 
+//Возведение числа в степень
 func Pow(base, exp int) int {
 	result := 1
 	for i := 0; i < exp; i++ {
