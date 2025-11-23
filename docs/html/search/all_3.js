5ff3ef0d6319d88492581a342e03f930c0d5911a
EMZemskova17 <katuusha0@yandex.ru>
2025-11-23 20:36:40 +0300

    docs fix

--- docs/html/search/all_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['newfeature_2ego_0',['newfeature.go',['../newfeature_8go.html',1,'']]]
-];
--- html/search/all_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['pow_0',['Pow',['../math_8go.html#a6a3ec2f12236580357fd45286fba3efd',1,'math.go']]]
-];
